f4da753ddba9c259d33e64947685b04e933aa161
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-02-14 02:30:26 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import ChatContainer from './components/ChatContainer';
 
 function App() {
   return (
-    <div style={{ backgroundColor: "#ece5dd", height: "100vh" , padding :10}}>
+    <div style={{ backgroundColor: "#ece5dd", height: "100vh", padding: 10 }}>
       <h2>Simple Chat App</h2>
       <ChatContainer />
     </div>
