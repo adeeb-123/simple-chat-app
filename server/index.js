232a3cfae768dd123a0eca0338ff17f49ee87fd8
Adeeb Ahmad <adeebsiddiqui77@gmail.com>
2024-02-14 03:52:44 +0530

    Update index.js

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const http = require('http')
+const Server = require("socket.io").Server
+const app = express()
+
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
+// when client connects to server
+io.on("connection", (socket) => {
+    console.log('We are Connected')
+
+    socket.on("chat", (chat) => {
+        io.emit('chat', chat)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('disconnected')
+    })
+})
+
+server.listen(3001, () => {
+    console.log("Listening at 3001")
+})
